2239b97279a3d60a095190ce211ddfd8dd3bcd4d
nezjutsu <seankral9@gmail.com>
2022-02-06 19:55:44 -0500

    inital files, created html route directories

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// make public directory a static resource
+app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}.`);
+});
