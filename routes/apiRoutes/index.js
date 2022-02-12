176fb4abe725b8ad2e53037ffc4a0a69fa38441e
nezjutsu <seankral9@gmail.com>
2022-02-12 18:14:09 -0500

    created post route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 const notes = require('../../db/db');
+const createNewNote = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
+})
 
 module.exports = router;
